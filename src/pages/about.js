63c7f3e90b7a06d4ffa02f2e660dda71610e5ceb
望月祐汰 <mczk9402@gmail.com>
2021-01-17 11:02:49 +0900

    create gatsby

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+// src/pages/about.js
+
+import React from 'react'
+
+const About = () => (
+  <div>
+    <h1>About Page</h1>
+    <div>This is a About Page</div>
+  </div>
+)
+
+export default About
